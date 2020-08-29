737a16eaa7ccf6663514949c813f04012539ce20
ShabbirHasan1 <shabbir.hasan@outlook.in>
2020-08-30 00:02:41 +0200

    updated

---
+++ target/rls/debug/deps/twtr-7c81218ac5a84dd8.d
@@ -0,0 +1,5 @@
+/home/n0ak95/Documents/coding/twtr/target/rls/debug/deps/twtr-7c81218ac5a84dd8.rmeta: src/main.rs
+
+/home/n0ak95/Documents/coding/twtr/target/rls/debug/deps/twtr-7c81218ac5a84dd8.d: src/main.rs
+
+src/main.rs:
---
+++ target/rls/debug/deps/twtr-7e7d31ac0d855ba5.d
@@ -0,0 +1,5 @@
+/home/n0ak95/Documents/coding/twtr/target/rls/debug/deps/twtr-7e7d31ac0d855ba5.rmeta: src/main.rs
+
+/home/n0ak95/Documents/coding/twtr/target/rls/debug/deps/twtr-7e7d31ac0d855ba5.d: src/main.rs
+
+src/main.rs:
